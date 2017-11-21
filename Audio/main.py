a2ae3b0b0462ee653109c3e9c4de6f08cfaec502
hoaxshark <hoaxshark@live.com>
2017-11-21 16:27:59 +0000

    trying to get combining audio files to work

--- Audio/main.py
+++ Audio/main.py
@@ -12,20 +12,24 @@ Window = (window_width, window_height)
 white = (255, 255, 255)
 green = (50, 160, 50)
 
+good_sound = pygame.mixer.Sound('Sounds/lake_bird_chipping.wav')
+bad_sound = pygame.mixer.Sound('Sounds/wind_howl.wav')
+
+good_sound_array = pygame.sndarray.samples(good_sound)
+bad_sound_array = pygame.sndarray.samples(bad_sound)
+
 screen = pygame.display.set_mode(Window)
 
-Cave = pygame.image.load("Images/cave.jpg")
-Cave = Cave.convert(32)
-Cave = pygame.transform.scale(Cave, (rect_width, rect_height))
+def img_transform(image_location,size):
+    """loads image converts to 32bit transforms to the size and returns the image"""
+    temp_img = pygame.image.load(image_location)
+    temp_img = temp_img.convert(32)
+    temp_img = pygame.transform.scale(temp_img, size)
+    return temp_img
 
-Forest = pygame.image.load("Images/forest.jpg")
-Forest = Forest.convert(32)
-Forest = pygame.transform.scale(Forest, (rect_width, rect_height))
-
-Town = pygame.image.load("Images/town.jpg")
-Town = Town.convert(32)
-Town = pygame.transform.scale(Town, (rect_width, rect_height))
-
+Town = img_transform("Images/town.jpg",(rect_width, rect_height))
+Forest = img_transform("Images/forest.jpg",(rect_width, rect_height))
+Cave = img_transform("Images/cave.jpg",(rect_width, rect_height))
 
 def text_surface(text, font, color):
     """Renders font and returns it and the rect
@@ -52,6 +56,16 @@ def choice_screen():
     text_place("Cave", text_on_button, white, (30 + (200/2)), (100 + 170))
     text_place("Forest", text_on_button, white, (290 + (200 / 2)), (100 + 170))
     text_place("Town", text_on_button, white, (550 + (200 / 2)), (100 + 170))
+
+#def append_sounds():
+
+def combine_sounds(sound1, sound2):
+    values=[]
+    for i in range(0,len(sound1)):
+        values[i] = sound1 + sound2
+    return values
+
+combine_sounds(good_sound_array,bad_sound_array)
 
 running = True
 choice_screen()
