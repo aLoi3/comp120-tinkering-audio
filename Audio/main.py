79767cc68213c62bd0e8c2555d186bb7af023d24
hoaxshark <hoaxshark@live.com>
2017-11-21 10:51:26 +0000

    added text_place function

--- Audio/main.py
+++ Audio/main.py
@@ -8,7 +8,7 @@ window_width = 800
 Window = (window_width, window_height)
 
 white = (255, 255, 255)
-green = (50, 200, 50)
+green = (50, 160, 50)
 
 screen = pygame.display.set_mode(Window)
 
@@ -20,16 +20,21 @@ def text_surface(text, font, color):
     return text_surf, text_surf.get_rect()
 
 
+def text_place(text, size, color, center_width, center_height):
+    text_surf, text_rect = text_surface(text, size, color)
+    text_rect.center = (center_width, center_height)
+    screen.blit(text_surf, text_rect)
+
+
 def choice_screen():
     large_text = pygame.font.SysFont('arial', 50)
     small_text = pygame.font.SysFont('arial', 25)
-    text_surf, text_rect = text_surface("choose your environment", small_text, white)
-    text_rect.center = ((window_width/2), (window_height/8))
-    screen.blit(text_surf, text_rect)
+    text_place("Choose your environment", small_text, white, window_width/2, window_height/8)
 
     pygame.draw.rect(screen, green, (30, 100, 200, 150))
-    pygame.draw.rect(screen, green, (285, 100, 200, 150))
-    pygame.draw.rect(screen, green, (540, 100, 200, 150))
+    pygame.draw.rect(screen, green, (290, 100, 200, 150))
+    pygame.draw.rect(screen, green, (550, 100, 200, 150))
+
 
 
 running = True
