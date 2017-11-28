a6b1fd3ecf390104bc56b67203bed4e42ce7b711
hoaxshark <hoaxshark@live.com>
2017-11-28 11:00:12 +0000

    added play_sound function and doc strings to tracks.py

--- Audio/tracks.py
+++ Audio/tracks.py
@@ -10,22 +10,33 @@ class Play:
     cave = []
     forest = []
 
-    def __init__(self):
+    def __init__(self, location):
         self.set_channels()
 
     def play_base(self, location):
+        """Selects the background sound for the current location"""
         base = pygame.mixer.Sound('Sounds/', location, '.wav')
         self.channel[0].play(base)
 
     def set_channels(self):
+        """Creates channels for the given max channels"""
         pygame.mixer.set_num_channels(self.max_channels)
         for i in range(0, self.max_channels - 1):
             self.channel[i] = pygame.mixer.Channel(i)
 
     def sound_select(self, location):
+        """selects a random sound from the array of sounds dependant on location"""
         random_number = random.randint(1,self.max_sounds - 1)
-        selected_sound = location[random_number]
+        selected_sound = pygame.mixer.Sound(location[random_number])
         return selected_sound
+
+    def play_sound(self, location, channel_number):
+        """Plays currently selected sound on the requested channel"""
+        current_sound = self.sound_select(location)
+        self.channel[channel_number].play(current_sound)
+
+
+
 
 
 
