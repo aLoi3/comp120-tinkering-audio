2acacd481f1ae2d1fec85f80bbc3aa9438a8baeb
Tomas Mazurkevic <mazurkevic.tomas@gmail.com>
2017-11-28 11:27:42 +0000

    * Maintainability: doc-strings, comments

--- Audio/main.py
+++ Audio/main.py
@@ -1,55 +1,65 @@
 import pygame
-import audioop
-import wave
 
+# Pygame initialisations
 pygame.init()
 pygame.mixer.init()
 
+# Variables
 rect_width = 200
 rect_height = 150
 window_height = 300
 window_width = 800
 Window = (window_width, window_height)
 
+# Different colors
 white = (255, 255, 255)
 green = (50, 160, 50)
 
 screen = pygame.display.set_mode(Window)
 
-#good_sound = pygame.mixer.Sound('Sounds/lake_bird_chipping.wav')
-#bad_sound = pygame.mixer.Sound('Sounds/wind_howl.wav')
-
-#good_sound_array = pygame.sndarray.samples(good_sound)
-#bad_sound_array = pygame.sndarray.samples(bad_sound)
-
-
 
 def img_transform(image_location, size):
-    """loads image converts to 32bit transforms to the size and returns the image"""
+    """
+    Loads image
+    Converts to 32bit
+    Transforms to the size and returns the image
+    """
     temp_img = pygame.image.load(image_location)
     temp_img = temp_img.convert(32)
     temp_img = pygame.transform.scale(temp_img, size)
     return temp_img
 
+# Sets images to variables that are called with img_transform function
+# for better understanding of a code
 Town = img_transform("Images/town.jpg", (rect_width, rect_height))
 Forest = img_transform("Images/forest.jpg", (rect_width, rect_height))
 Cave = img_transform("Images/cave.jpg", (rect_width, rect_height))
 
 
 def text_surface(text, font, color):
-    """Renders font and returns it and the rect
-    inspiration taken from https://pythonprogramming.net/displaying-text-pygame-screen/"""
+    """
+    Renders font and returns it and the rect - inspiration taken from
+    https://pythonprogramming.net/displaying-text-pygame-screen/
+    """
     text_surf = font.render(text, True, color)
     return text_surf, text_surf.get_rect()
 
 
 def text_place(text, size, color, center_width, center_height):
+    """
+    Replaces the given text to the center
+    """
     text_surf, text_rect = text_surface(text, size, color)
     text_rect.center = (center_width, center_height)
     screen.blit(text_surf, text_rect)
 
 
 def choice_screen():
+    """
+    Places a text on top of a screen for clarity.
+    Replaces rectangles with an appropriate image - cave, forest and town.
+    Places text below images, describing what environment that is.
+    """
     title = pygame.font.SysFont('arial', 50)
     text_on_button = pygame.font.SysFont('arial', 35)
     text_place("Choose your environment", title, white, window_width/2, window_height/8)
@@ -58,20 +68,11 @@ def choice_screen():
     pygame.Surface.blit(screen, Forest, (290, 100, rect_width, rect_height))
     pygame.Surface.blit(screen, Town, (550, 100, rect_width, rect_height))
 
-    text_place("Cave", text_on_button, white, (30 + (200/2)), (100 + 170))
-    text_place("Forest", text_on_button, white, (290 + (200 / 2)), (100 + 170))
-    text_place("Town", text_on_button, white, (550 + (200 / 2)), (100 + 170))
+    text_place("Cave", text_on_button, white, (30 + (rect_width/2)), (100 + 170))
+    text_place("Forest", text_on_button, white, (290 + (rect_width / 2)), (100 + 170))
+    text_place("Town", text_on_button, white, (550 + (rect_width / 2)), (100 + 170))
 
-#def append_sounds():
-
-
-#def combine_sounds(sound1, sound2):
-#    values = []
-#    for i in range(0, len(sound1)):
-#        sound1 =
-#    return values
-
-
+# Main loop
 running = True
 choice_screen()
 
@@ -82,5 +83,3 @@ while running:
     pygame.display.update()
 pygame.quit()
 
-
-#combine_sounds(good_sound_array, bad_sound_array)
